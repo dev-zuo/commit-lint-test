7d954b5d0bb112786308f83741c60fdd7102da1d
zuoxiaobai <guoqzuo@gmail.com>
2023-06-11 10:44:27 +0800

    upd: update func

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2, // Level  0 disable、1 warning、2 error
+            'always', // always|never
+            ['upd', 'feat', 'fix',  'docs','style',  'refactor', 'perf', 'test',  'build', 'ci', 'chore','revert',]
+        ]
+    }
+}
