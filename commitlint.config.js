b643479296fc8a1d9eb56a97c24b1a1cb62f6bfa
zuoxiaobai <guoqzuo@gmail.com>
2023-06-11 10:53:50 +0800

    upd: update config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2, // Level  0 disable、1 warning、2 error
-            'always', // always|never
-            ['upd', 'feat', 'fix',  'docs','style',  'refactor', 'perf', 'test',  'build', 'ci', 'chore','revert',]
-        ]
-    }
-}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2, // Level  0 disable、1 warning、2 error
+      "always", // always|never
+      [
+        "upd",
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ], // 默认 11 个类型，这里新增一个 upd 类型
+    ],
+  },
+};
