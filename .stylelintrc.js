c45045115d1d945e48c4d56aa77786e10b342dc1
zuoxiaobai <guoqzuo@gmail.com>
2023-06-17 10:58:49 +0800

    feat: update pre-commit

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,16 @@
-module.exports = { 
-    "extends": [
-        "stylelint-config-standard", // 普通css，默认
-        "stylelint-config-standard-scss", // scss 支持
-    ],
-    "rules": {
-        "max-nesting-depth": 2 // 样式最大嵌套层数，总共最多 3 层
+module.exports = {
+  extends: [
+    "stylelint-config-standard", // 普通css，默认
+    "stylelint-config-standard-scss", // scss 支持
+  ],
+  rules: {
+    "max-empty-lines": 1, // css 最大空白行 1
+    "max-nesting-depth": 2, // 样式最大嵌套层数，总共最多 3 层
+  },
+  overrides: [
+    {
+      files: ["**/*.{html,vue}"],
+      customSyntax: "postcss-html", // 支持 HTML 包括 Vue SFC
     },
-    "overrides": [
-        {
-          "files": ["*.vue", "*.html"],
-          "customSyntax": "postcss-html" // 支持 HTML 包括 Vue SFC
-        }
-      ]
-}
+  ],
+};
